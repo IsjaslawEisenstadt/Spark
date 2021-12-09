05db287a96638698a3da95eeff6585446f1bc109
Chris9898 <c.hoerath@gmx.net>
2021-12-09 20:27:44 +0100

    add selectable missions to MissionSelection

---
+++ Assets/Scripts/Menu/MissionSelection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MissionSelection : MonoBehaviour
+{
+	public SceneLoader sceneLoader;
+	public GameObject entry;
+	public List<Mission> missions;
+
+	void Start()
+	{
+		GameObject entryInstance;
+		for (int i = 0; i < missions.Count; i++)
+		{
+			entryInstance = Instantiate(entry, transform);
+			entryInstance.transform.GetChild(0).GetComponent<TMP_Text>().text = (i + 1).ToString();
+			entryInstance.transform.GetChild(2).GetComponent<TMP_Text>().text = missions[i].name;
+			int index = i;
+			entryInstance.GetComponent<Button>().onClick.AddListener(delegate { OnSelectMission(index); });
+		}
+	}
+
+	public void OnSelectMission(int index)
+	{
+		CurrentMission.currentMission = missions[index];
+		sceneLoader.SwitchScene("MissionMode");
+	}
+}
