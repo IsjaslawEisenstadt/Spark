a3abcca87e371efd9cbd4304ceea398faa1c02cb
Marco <xxkuriousxx@gmx.net>
2021-12-20 22:21:52 +0100

    hotfix show HUD only in MissionMode

--- Assets/Scripts/HUD/AvailableGates.cs
+++ Assets/Scripts/HUD/AvailableGates.cs
@@ -1,20 +1,21 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AvailableGates : MonoBehaviour
 {
 	[SerializeField] GameObject gatePanelPrefab;
 	void Start()
 	{
+		if (SceneManager.GetActiveScene().name != "MissionMode")
+			return;
+
 		Mission currentMission = CurrentMission.missions[CurrentMission.currentMissionIndex];
-		GameObject gatePanelInstance;
 
 		foreach (KeyValuePair<GateType, int> entry in currentMission.GateRestriction)
 		{
-			gatePanelInstance = Instantiate(gatePanelPrefab, transform);
+			GameObject gatePanelInstance = Instantiate(gatePanelPrefab, transform);
 			GameObject gateName = gatePanelInstance.transform.GetChild(0).gameObject;
 			GameObject gateCount = gatePanelInstance.transform.GetChild(1).gameObject;
 			gateName.GetComponent<TextMeshProUGUI>().text = entry.Key.ToString();
