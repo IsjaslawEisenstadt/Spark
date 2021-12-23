af7dd335e6d446ce0852399ff859f902a2796ec4
Christian <c.hoerath@gmx.net>
2021-12-23 22:59:50 +0100

    fix touch behavior

--- Assets/Scripts/Desk/GateSelector.cs
+++ Assets/Scripts/Desk/GateSelector.cs
@@ -5,27 +5,45 @@ public class GateSelector : MonoBehaviour
 {
 	public GameObject CurrentSelectedObject { get; private set; }
 
+	private bool touchStartOverUI;
+	private bool isTouching;
+
 	void Update()
 	{
-		if (Input.touchCount <= 0 || EventSystem.current.IsPointerOverGameObject(0))
-		{
+		if (Input.touchCount <= 0)
 			return;
-		}
 
 		Touch touchEvent = Input.GetTouch(0);
 
+		if (Input.GetTouch(0).phase == TouchPhase.Began)
+			return;
+
+		if (!isTouching)
+		{
+			isTouching = true;
+			touchStartOverUI = EventSystem.current.IsPointerOverGameObject(0);
+			Debug.LogWarning("TouchStartet with touchStartOverUI: " + touchStartOverUI);
+		}
+
 		if (touchEvent.phase == TouchPhase.Ended)
 		{
-			(bool successful, GameObject hitObject) = RayCaster.Instance.GetHitObject();
-
-			if (successful && (hitObject.CompareTag("LogicGate") || hitObject.CompareTag("Source") ||
-			                   hitObject.CompareTag("Sink")))
+			if (!(touchStartOverUI || EventSystem.current.IsPointerOverGameObject(0)))
 			{
-				CurrentSelectedObject = hitObject;
-				UIManager.Instance.OnGateSelected();
+				Debug.LogWarning("Cast");
+				(bool successful, GameObject hitObject) = RayCaster.Instance.GetHitObject();
+
+				if (successful && (hitObject.CompareTag("LogicGate") || hitObject.CompareTag("Source") ||
+									hitObject.CompareTag("Sink")))
+				{
+					CurrentSelectedObject = hitObject;
+					UIManager.Instance.OnGateSelected();
+				}
+				else
+					UIManager.Instance.OnNoGateSelected();
 			}
-			else
-				UIManager.Instance.OnNoGateSelected();
+
+			touchStartOverUI = isTouching = false;
+			Debug.LogWarning("Touch ends");
 		}
 	}
 }
