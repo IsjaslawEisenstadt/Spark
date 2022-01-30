adcd3dfde75402896fffb9c03c1348b2258ca2ed
Chris9898 <c.hoerath@gmx.net>
2022-01-30 14:52:16 +0100

    add tutorial mission

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+	public SceneLoader sceneLoader;
+	public Mission mission;
+
+	public void OnTutorial()
+	{
+		CurrentMission.missions = new List<Mission>();
+		CurrentMission.missions.Add(mission);
+		CurrentMission.currentMissionIndex = 0;
+		sceneLoader.SwitchScene("TutorialMode");
+	}
+}
